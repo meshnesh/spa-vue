9e999a51b0fdd3324c5facc35ee0608a77a9282a
Antony Ng'ang'a <antony_mn@Antonys-MBP.mshome.net>
2017-08-30 02:16:09 +0300

    initial commit of the project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-link v-bind:to="'/'">Home</router-link>
+    <router-link v-bind:to="'/about'">About</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
