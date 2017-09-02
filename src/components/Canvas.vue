4ed6f7c87ad401a0369d6cb70fff56b3ce289a49
Antony Ng'ang'a <antony_mn@Antonys-MBP.mshome.net>
2017-09-02 19:45:05 +0300

    separated main and side bar views.

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -6,16 +6,25 @@
         <div class="col-sm-8 blog-main">
 
           <div class="blog-post">
-            <h2 class="blog-post-title">Sample blog post</h2>
-            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+            <div class="card mb-3 rounded-0 border-0">
+              <img class="card-img-top rounded" src="../assets/sky.jpg" alt="Card image cap">
+              <div class="card-body">
+                 <h3>Adventure</h3>
+                <h4 class="blog-post-title card-title">FALLING</h4>
+                <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
 
-            <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+            <hr/>
+            <!-- <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
             <hr>
             <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
             <blockquote>
               <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
             </blockquote>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p> -->
             <h2>Heading</h2>
             <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
             <h3>Sub-heading</h3>
@@ -45,10 +54,26 @@
 
         </div><!-- /.blog-main -->
 
-        <div class="col-sm-3 offset-sm-1 blog-sidebar">
+        <div class="col-sm-4 offset-sm-2 blog-sidebar">
           <div class="sidebar-module sidebar-module-inset">
-            <h4>About</h4>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <div class="line-across">
+              <h4><span>About</span></h4>
+            </div>
+
+            <div class="card border-0" style="width: auto;">
+              <img class="card-img-top rounded" src="../assets/parekh.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <ul class="social-media">
+                  <li><a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                  <li><a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                  <li><a href=""><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+                  <li><a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                  <li><a href=""><i class="fa fa-tumblr" aria-hidden="true"></i></a></li>
+                  <li><a href=""><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+                </ul>
+              </div>
+            </div>
           </div>
           <div class="sidebar-module">
             <h4>Archives</h4>
@@ -67,14 +92,14 @@
               <li><a href="#">April 2013</a></li>
             </ol>
           </div>
-          <div class="sidebar-module">
+          <!-- <div class="sidebar-module">
             <h4>Elsewhere</h4>
             <ol class="list-unstyled">
               <li><a href="#">GitHub</a></li>
               <li><a href="#">Twitter</a></li>
               <li><a href="#">Facebook</a></li>
             </ol>
-          </div>
+          </div> -->
         </div><!-- /.blog-sidebar -->
 
       </div><!-- /.row -->
